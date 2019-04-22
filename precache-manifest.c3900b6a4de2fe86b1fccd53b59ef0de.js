954593a07bcd4b440805b3048fcb55c9d28d2eb8
Shayan <shayanshaikh996@gmail.com>
2019-04-22 14:45:31 +0500

    first build

---
+++ precache-manifest.c3900b6a4de2fe86b1fccd53b59ef0de.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = [
+  {
+    "revision": "787fd717fd7bf8fccc815a9f405ad635",
+    "url": "./static/media/tec-network.787fd717.png"
+  },
+  {
+    "revision": "f4a3ddd6fc2fb459a8ce",
+    "url": "./static/css/main.e4dedd30.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "./static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c477984f09d8730a9816",
+    "url": "./static/js/2.a90f34ca.chunk.js"
+  },
+  {
+    "revision": "74de5d224129d2356c1adcfee0021d1b",
+    "url": "./static/media/tez-blue.74de5d22.png"
+  },
+  {
+    "revision": "14bdae587d5fea0baf821e32c254043d",
+    "url": "./static/media/sli2.14bdae58.jpg"
+  },
+  {
+    "revision": "7bc385442299ff0f37883ccf843f28dd",
+    "url": "./static/media/ss.7bc38544.png"
+  },
+  {
+    "revision": "de904e7fab81aecc563419b71ee19dfd",
+    "url": "./static/media/shayan.de904e7f.jpg"
+  },
+  {
+    "revision": "28f989db763317880170ee9f01cc0570",
+    "url": "./static/media/nadeem.28f989db.jpg"
+  },
+  {
+    "revision": "1ee3b9f03ad0d1de76819dd8437d4123",
+    "url": "./static/media/humza.1ee3b9f0.jpg"
+  },
+  {
+    "revision": "7b8d5c2eaa421acc47023a558de7e90b",
+    "url": "./static/media/naureen.7b8d5c2e.jpg"
+  },
+  {
+    "revision": "2a053dcc0c569615a41b2ebdcac0e5a7",
+    "url": "./static/media/shoaib.2a053dcc.jpg"
+  },
+  {
+    "revision": "4e8f779573c4b47c606083984f3cdb4b",
+    "url": "./static/media/jamal.4e8f7795.jpg"
+  },
+  {
+    "revision": "e434b5d80f93b9b89c21859ac9140461",
+    "url": "./static/media/shafiq.e434b5d8.jpg"
+  },
+  {
+    "revision": "f9da8da99ea5b1caf917a08cac53f23c",
+    "url": "./static/media/adeel.f9da8da9.jpg"
+  },
+  {
+    "revision": "0f2803f742bcf0c09f2fb4e78d534944",
+    "url": "./static/media/laiba.0f2803f7.jpg"
+  },
+  {
+    "revision": "0d030576386712d0fc240d5eeedab773",
+    "url": "./static/media/planet-n.0d030576.png"
+  },
+  {
+    "revision": "2351e7695ee5f6ea0c0e1d65745c4cf3",
+    "url": "./static/media/omidyar.2351e769.png"
+  },
+  {
+    "revision": "4c5a469df0c58bd85a7954ca9f623783",
+    "url": "./static/media/accion.4c5a469d.png"
+  },
+  {
+    "revision": "f1729391bf1f0c72d93374d7d243a842",
+    "url": "./static/media/hand-mobile.f1729391.png"
+  },
+  {
+    "revision": "975db6330d8ab18f6de17d4bdf84a3bd",
+    "url": "./static/media/tec-tree.975db633.png"
+  },
+  {
+    "revision": "af3cbb89b924aa36a207b8f12daeccfe",
+    "url": "./static/media/tez-wallet.af3cbb89.jpg"
+  },
+  {
+    "revision": "f4a3ddd6fc2fb459a8ce",
+    "url": "./static/js/main.362b67a5.chunk.js"
+  },
+  {
+    "revision": "3720659373df4fb5224de06788662ff1",
+    "url": "./static/media/career-women.37206593.png"
+  },
+  {
+    "revision": "0ac7ed9d230300d21b53166623597046",
+    "url": "./static/media/career-team.0ac7ed9d.gif"
+  },
+  {
+    "revision": "5dd8ea241ce30aeaf2ae76d124276960",
+    "url": "./static/media/career-learn.5dd8ea24.png"
+  },
+  {
+    "revision": "33c163a2df1639bcc8c69d3fc40082f3",
+    "url": "./static/media/career-success.33c163a2.png"
+  },
+  {
+    "revision": "d629a8a45ad23ace58dec80e4f6e4591",
+    "url": "./static/media/career-grow.d629a8a4.png"
+  },
+  {
+    "revision": "5459785f404f705de56d0ec71e01fd7f",
+    "url": "./static/media/sli3.5459785f.jpg"
+  },
+  {
+    "revision": "dd493aed7e230bbcf057b983777ce450",
+    "url": "./static/media/cTeam.dd493aed.jpg"
+  },
+  {
+    "revision": "73dc8df34a807297921bba5803601f73",
+    "url": "./static/media/tez-loader.73dc8df3.gif"
+  },
+  {
+    "revision": "e1086d5360c406062badf93fcbf86b71",
+    "url": "./static/media/cell.e1086d53.png"
+  },
+  {
+    "revision": "3f1c01d43018fda1ef4f7bd2daf102f6",
+    "url": "./static/media/fb.3f1c01d4.png"
+  },
+  {
+    "revision": "c013dae7ef65f49c52a9c16ed47ab282",
+    "url": "./static/media/footer-img.c013dae7.png"
+  },
+  {
+    "revision": "8e2b6d4af3c41cb63d49876ac351b4aa",
+    "url": "./static/media/tweet.8e2b6d4a.png"
+  },
+  {
+    "revision": "cc3525c0096c2c90af4e784b6af889ab",
+    "url": "./static/media/in.cc3525c0.png"
+  },
+  {
+    "revision": "d5596ad008b02d2854d88b13bddfde6f",
+    "url": "./static/media/play.d5596ad0.png"
+  },
+  {
+    "revision": "7cc8cbbd7a330c0d1e7c08c6d62711e4",
+    "url": "./static/media/leaguespartan-bold.7cc8cbbd.ttf"
+  },
+  {
+    "revision": "3812d86fa4f162846016e03340c94427",
+    "url": "./static/media/leaguespartan-bold.3812d86f.woff2"
+  },
+  {
+    "revision": "de124ab4fe636436b7f9d3b1f5ac3eed",
+    "url": "./static/media/leaguespartan-bold.de124ab4.woff"
+  },
+  {
+    "revision": "5d202a7f44e80632637a8f06f374147e",
+    "url": "./static/media/leaguespartan-bold.5d202a7f.eot"
+  },
+  {
+    "revision": "3c33ca150ccb893b3f89fabce784e09a",
+    "url": "./static/media/leaguespartan-bold.3c33ca15.svg"
+  },
+  {
+    "revision": "9dbbf063721f49919e6f714574087f07",
+    "url": "./static/media/bg-pattern.9dbbf063.png"
+  },
+  {
+    "revision": "feb82c0a38413a0f7c114dd1eb694133",
+    "url": "./static/media/bg-white.feb82c0a.png"
+  },
+  {
+    "revision": "ad1518ba7fe9f45e910f066c5db23e79",
+    "url": "./index.html"
+  }
+];
